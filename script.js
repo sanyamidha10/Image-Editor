3a40af5f62d0f0c576f369966cb239861484feae
Sanya Midha <sanyamidha.10@gmail.com>
2023-04-11 00:21:57 +0530

    Choose Image Button

---
+++ script.js
@@ -0,0 +1,19 @@
+const fileInput = document.querySelector('.file-input'),
+previewImg = document.querySelector('.preview-img img'),
+chooseImgBtn = document.querySelector('.choose-img');
+
+const loadImage = () =>{
+    let file = fileInput.files[0]; //getting user selected file
+    if(!file) return;
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", ()=>{
+        document.querySelector(".container").classList.remove("disable");
+    })
+    
+}
+
+fileInput.addEventListener("change", loadImage);
+
+chooseImgBtn.addEventListener("click", ()=>{
+    fileInput.click();
+})
